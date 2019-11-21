0d2b0907f0d32b6cb4d8458c8567b052bf81e38e
Silva <walme004@gmail.com>
2019-11-21 06:57:01 -0500

    add ”T-Shirt Info” section

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,18 @@ const $paymentSelected = $("select[id=payment]");
 const $submitButton = $("button[type=submit]");
 const $jobTitleSelected = $("select[id=title]");
 const $otherTitle = $("input[id='other-title']");
+const $tShirtUserDesign = $("select[id=design]");
+const $tshirtCurrentColorOption = $("select[id=color]");
+const $tshirtOriginalColorOption = $tshirtCurrentColorOption.children();
+const $tshirtDefaultColorOption = $('<option>Please select a T-shirt theme</option>');
+
 
 function main() {
-    //Set focus on the first text field
-    $("input[type=text]").first().focus();
-    
+    ////Set focus on the first text field
+    $("input[type=text]").first().focus();    
+
+    //
+    $tshirtCurrentColorOption.html($tshirtDefaultColorOption);
 }
 
 function executeValidator(validator,$input) {
@@ -53,6 +60,24 @@ $userEmailInput.on("input blur", createListener(isValidEmail));
 
 $jobTitleSelected.on("input", () => {
     $jobTitleSelected.val() === 'other' ? $otherTitle.show() : $otherTitle.hide();
+});
+
+//”T-Shirt Info” section
+$tShirtUserDesign.on("input", () => {
+    let designSelected = $tShirtUserDesign.children("option:selected").text().toLowerCase();
+    designSelected = designSelected.replace(/Theme\s*-\s*(\w)/i, '$1');
+    const $colors = $('<select></select>');
+    //find matching
+    $tshirtOriginalColorOption.each(function () {
+        const currColor = $(this).text().toLowerCase();
+        if (currColor.includes(designSelected)) $colors.append($(this));
+    });
+
+    if ($colors.children().length === 0 )
+        $tshirtCurrentColorOption.html($tshirtDefaultColorOption);
+    else
+        $tshirtCurrentColorOption.html($colors.children());
+
 });
 
 document.addEventListener('DOMContentLoaded', () => { main(); });
